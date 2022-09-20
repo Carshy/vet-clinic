d19c690b185d4c7bf60a1216b909d341760e2f9d
Collins <mussocollins@gmail.com>
2022-09-20 15:56:08 +0300

    Create development

---
+++ data.sql
@@ -0,0 +1,6 @@
+/* Populate database with sample data. */
+
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutured, weight_kg) VALUES('Agumon', DATE '2020-02-03', '0', 'true', '10.23');
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutured, weight_kg) VALUES('Gabumon', DATE '2018-11-15', '2', 'true', '8');
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutured, weight_kg) VALUES('Pikachu', DATE '2021-01-07', '1', 'false', '15.04');
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutured, weight_kg) VALUES('Devimon', DATE '2017-05-12', '5', 'true', '11.0');
