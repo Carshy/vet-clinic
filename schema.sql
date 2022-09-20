7761a03d1be6e1eedcf9cf1f28622dbe22838f64
Collins <mussocollins@gmail.com>
2022-09-20 15:51:06 +0300

    Create Database

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE vet_clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals(
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts int NOT NULL,
+    neutured boolean NOT NULL,
+    weight_kg decimal NOT NULL
+ );
